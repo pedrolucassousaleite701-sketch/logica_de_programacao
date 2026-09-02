599bbf7bab730b5deadd3833913f261f42b5ebc5
pedrolucassousaleite701 <pedrolucassousaleite701@gmail.com>
2026-09-02 17:59:32 -0300

    aprendendo sobre variaveis

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+// comentário de l linhas só
+/*
+comentario
+de várias
+linhas
+*/
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author PedroLeite
+ */
+public class Variaveis {
+    // a classe manin é quem executa os codigos
+    public static void main(String[] args) {
+  /* código de saida
+   - são utilizados para imprimir/mostrar
+      dados e mensagens
+        */
+  System.out.println("Ola");
+  System.out.println("eu sou um programador");
+   // println - pula linha / print - não pula
+  System.out.println("Eu ja sei programar");
+  System.out.println(",eu amo java");
+  
+  //CRIAÇÃO DE VARIÁVEIS
+  int idade = 24;//valor inteiro
+  double altura = 1.63;//valor decimal grande 
+  float peso ; // valor decimal pequeno
+  String nome = "jhessik keally";//textos
+  boolean casada = false; //valor lógico
+  // true - verdadeiro , false - falso
+  System.out.println("ola"+nome);
+  System.out.println("voce tem"+idade+" anos de idade");
+  
+   }// fim da manin 
+}
+// nenhuma código pode ficar aqui  fora
